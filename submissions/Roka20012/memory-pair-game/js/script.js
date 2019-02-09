6e333d0320ac9b758c712ab6812f2cd1f3601515
Rostyk Broslavsky <broslavsky.rostislaw@gmail.com>
2019-02-09 16:56:10 +0200

    add some improvents

--- submissions/Roka20012/memory-pair-game/js/script.js
+++ submissions/Roka20012/memory-pair-game/js/script.js
@@ -1,19 +1,25 @@
-const delayForAnswer = 1000;
-const delay = 500;
-const twoCards = 2;
-const winGame = 12;
+"use strict";
 
+const DELAY_FOR_ANSWER = 1000;
+const TWO_CARDS = 2;
+const WIN_GAME = 12;
+
+let delay = 500;
 let flipper = document.querySelector(".flipper");
 let allCards = document.querySelectorAll(".flipper > div");
 
+document.querySelector(".flipper ~ img").addEventListener("load",() => {
+    showCards();
+});
+
 function showCards() {
-    setTimeout(function() {
+    setTimeout(() => {
         allCards.forEach(function(el) {
             el.classList.toggle("check");
         });
     }, delay);
 
-    setTimeout(function() {
+    setTimeout(() => {
         allCards.forEach(function(el) {
             el.classList.toggle("check");
         });
@@ -21,11 +27,28 @@ function showCards() {
 }
 
 function addCards() {
+    let cards = [];
+    cards = cards.concat(shuffleCards(), shuffleCards());
+    let fragment = document.createDocumentFragment();
+
+    cards.forEach((el, i) => {
+        let div = document.createElement("div");
+        let img = document.createElement("img");
+        img.setAttribute("draggable", false);
+        img.src = el;
+        div.appendChild(img);
+        fragment.appendChild(div);
+    });
+
+    flipper.appendChild(fragment);
+}
+
+function changeCards() {
     let img = document.querySelectorAll(".flipper div img");
     let cards = [];
     cards = cards.concat(shuffleCards(), shuffleCards());
 
-    cards.forEach(function(el, i) {
+    cards.forEach((el, i) => {
         img[i].src = el;
     });
 }
@@ -35,68 +58,69 @@ function shuffleCards() {
         "images/man4.jpg", "images/man5.jpg", "images/man6.jpg"
     ];
 
-    return cards.sort(function() {
+    return cards.sort(() => {
         return 0.5 - Math.random();
     });
+}
+
+function restartGame(restart, hideCards) {
+    return function() {
+        restart.remove();
+
+        hideCards.forEach((el) => {
+            el.classList.remove("hide");
+            el.classList.toggle("showme");
+        });
+
+        setTimeout(() => {
+            changeCards();
+            showCards();
+        }, delay);
+
+        setTimeout(() => {
+            allCards.forEach(function(el) {
+                el.classList.toggle("showme");
+            });
+        }, delay * 2);
+    };
 }
 
 function checkGameWin() {
     let hideCards = document.querySelectorAll(".hide");
 
-    if (hideCards.length === winGame) {
+    if (hideCards.length === WIN_GAME) {
         let restart = document.createElement("div");
 
         restart.innerHTML = "!!!YOU WIN!!!\n<div class='win'>RESTART</div>";
         restart.classList.add("win-game");
         document.body.appendChild(restart);
 
-        document.querySelector(".win").onclick = function() {
-            restart.remove();
-
-            hideCards.forEach(function(el) {
-                el.classList.remove("hide");
-                el.classList.toggle("showme");
-            });
-
-            setTimeout(function() {
-                addCards();
-                showCards();
-            }, delay);
-
-            setTimeout(function() {
-                allCards.forEach(function(el) {
-                    el.classList.toggle("showme");
-                });
-            }, delay * 2);
-        };
+        document.querySelector(".win").addEventListener("click", restartGame(restart, hideCards));
     }
 }
 
 function checkTwoCards() {
     let checkCards = document.querySelectorAll(".check img");
-    if (checkCards.length === twoCards) {
+    if (checkCards.length === TWO_CARDS) {
         if (checkCards[0].src === checkCards[1].src) {
-            setTimeout(function() {
+            setTimeout(() => {
                 checkCards[0].parentNode.classList.add("hide");
                 checkCards[1].parentNode.classList.add("hide");
                 checkCards[0].parentNode.classList.toggle("check");
                 checkCards[1].parentNode.classList.toggle("check");
             }, delay);
 
-            setTimeout(function() {
+            setTimeout(() => {
                 checkGameWin();
-            }, delayForAnswer);
+            }, DELAY_FOR_ANSWER);
         } else {
-            setTimeout(function() {
+            setTimeout(() => {
                 checkCards[0].parentNode.classList.toggle("check");
                 checkCards[1].parentNode.classList.toggle("check");
             }, delay);
         }
     }
 }
-
-addCards();
-showCards();
 
 function GameProces({ target }) {
     if (target.tagName === "IMG") {
