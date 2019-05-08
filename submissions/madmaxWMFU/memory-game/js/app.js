e6fc05f4168dcba0f2f4a3366a91098383f118d7
madmaxWMFU <yasmaks@gmail.com>
2019-05-08 13:59:19 +0300

    deleted styling elements, rename values, toggle()

--- submissions/madmaxWMFU/memory-game/js/app.js
+++ submissions/madmaxWMFU/memory-game/js/app.js
@@ -1,75 +1,73 @@
-const main = document.querySelector(".game-field");
-const arrayOfCards = ["card0", "card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13", "card14", "card15", "card16", "card17", "card18", "card19", "card20", "card21", "card22", "card23", "card24", "card25", "card26", "card27", "card28", "card29", "card30", "card31", "card32", "card33", "card34", "card35", "card36", "card37", "card38", "card39", "card40", "card41", "card42", "card43", "card44", "card45", "card46", "card47", "card48", "card49", "card50", "card51", "card52", "card53"];
-let twoSelectedCads = [];
+const gameField = document.querySelector(".game-field");
+let openCards = [];
 
-const shuffled = (arr) => arr.sort(() => 0.5 - Math.random());
+const arrayOfCards = (() => {
+	let cards = [];
+	for(let i = 0; i <= 53; i++) {
+		cards.push(`card${i}`);
+	} 
+	return cards;
+})();
 
-const drawCards = arrayOfCards => {
-	let selected = shuffled(arrayOfCards).slice(0, 10);
-	let playCards = shuffled([...selected, ...selected]);
-	playCards.forEach(val => main.insertAdjacentHTML("beforeEnd", getTemplateWrap(val)));
-}
+const getShuffledCards = (arr) => arr.sort(() => 0.5 - Math.random());
 
-const getTemplateWrap = (newClass) => {
+const getTemplateCard = (newClass) => {
     return `
-        <div class="flipper hideCard">
+        <div class="flipper hide-card">
             <div class="front"></div>
             <div class="back ${newClass}" data-value="${newClass}"></div>
         </div>`;
 }
 
-const hideAllSelectedCards = () => document.querySelectorAll(".flipper").forEach(item => hideCard(item));
-const hideCheckedCards = () => document.querySelectorAll(".rotateCard").forEach(item => item.style.visibility = "hidden");
-
-const hideCard = item => {
-	item.classList.remove("rotateCard");
-	item.classList.add("hideCard");
+const drawCards = (arrayOfCards) => {
+	let selected = getShuffledCards(arrayOfCards).slice(0, 10);
+	let playCards = getShuffledCards([...selected, ...selected]);
+	playCards.forEach((card) => gameField.insertAdjacentHTML("beforeEnd", getTemplateCard(card)));
 }
 
-const rotateCard = (item) => {
-	item.classList.remove("hideCard");
-	item.classList.add("rotateCard");
-}
+const hideAllSelectedCards = () => document.querySelectorAll(".flipper").forEach((item) => item.classList.remove("rotate-card"));
+const hideCheckedCards = () => document.querySelectorAll(".rotate-card").forEach((item) => item.classList.add("remove-card"));
+
+const showCard = (item) => item.classList.toggle("rotate-card");
 
 const countSuccessfullCard = () => {
-	if(document.querySelectorAll("[style ^='visibility']").length == 20) {
+	if(document.querySelectorAll(".remove-card").length == 20) {
     	alert("Congratulations! You found all the cards.");
     	init();
 	}
 }
 
-const checkCards = twoSelectedCard => {
-	let unique = twoSelectedCard.filter((el, i, a) => i === a.indexOf(el));
+const checkCards = (selectedCards) => {
+	let unique = selectedCards.filter((el, i, a) => i === a.indexOf(el));
 	if(unique.length == 1) {
 		setTimeout(() => {
 			hideCheckedCards();
-			twoSelectedCads = [];
+			openCards = [];
 			countSuccessfullCard();
 		}, 1000);
 	} else {
 		setTimeout(() => {
 			hideAllSelectedCards();
-			twoSelectedCads = [];
+			openCards = [];
 		}, 1000);
 	}
 }
 
 const init = () => {
-	document.querySelector(".game-field").innerHTML = "";
+	gameField.innerHTML = "";
 	drawCards(arrayOfCards);
-	document.querySelector(".game-field").addEventListener("click", e => {
-		if(e.target.parentNode.classList.contains("flipper")) {
-			if(twoSelectedCads.length < 2) {
-				rotateCard(e.target.parentNode);
-			    twoSelectedCads.push(e.target.nextElementSibling.dataset.value);
+	gameField.addEventListener("click", (e) => {
+		if(e.target.parentNode.classList.contains("flipper") && !e.target.parentNode.classList.contains("rotate-card")) {
+			if(openCards.length < 2) {
+				showCard(e.target.parentNode);
+			    openCards.push(e.target.nextElementSibling.dataset.value);
 		    } 
 
-		    if(twoSelectedCads.length == 2) {
-			    checkCards(twoSelectedCads);
+		    if(openCards.length == 2) {
+			    checkCards(openCards);
 		    }
 		}
 	})	
-		
 }
 
 window.onload = init();
