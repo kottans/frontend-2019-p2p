5f014848fdfba8f384d6cc0893f4df2d6d941dd4
homePC <aymkin@gmail.com>
2019-06-06 16:55:51 +0300

    second itertiion with @OleksiyRudenko in progress

--- submissions/aymkin/OOP-JS (tjsw)/index.js
+++ submissions/aymkin/OOP-JS (tjsw)/index.js
@@ -28,6 +28,17 @@ Inhobitant.prototype.getBio = function() {
 	}</strong>. ${this.getLimbs()}. ${this.getFriend()} \nEvery day I say: ${this.makeSound()}.`;
 };
 
+Inhobitant.prototype.getLimbs = function() {
+	let limbs = 'The limbs are not defined on my constructor';
+	if (this.hasOwnProperty('legs')) {
+		limbs = `I have ${this.legs} legs`;
+	}
+	if (this.hasOwnProperty('hands')) {
+		limbs += ` and ${this.hands} hands`;
+	}
+	return limbs;
+};
+
 function Animal(species, name, gender, say, friends) {
 	Inhobitant.call(this, species, name, gender, say, friends);
 	this.legs = 4;
@@ -35,28 +46,33 @@ function Animal(species, name, gender, say, friends) {
 
 Animal.prototype = Object.create(Inhobitant.prototype);
 
-Animal.prototype.getLimbs = function() {
-	return `I have ${this.legs} legs`;
-};
-
-function Human(species, name, gender, say, friends) {
-	Inhobitant.call(this, species, name, gender, say, friends);
+function Human(name, gender, say, friends) {
+	Inhobitant.call(this, name, gender, say, friends);
 	this.legs = 2;
 	this.hands = 2;
+	this.species = 'human';
 }
 
 Human.prototype = Object.create(Inhobitant.prototype);
 
-Human.prototype.getLimbs = function() {
-	return `I have ${this.legs} legs and ${this.hands} hands`;
+function Bird(species, name, gender, say) {
+	Inhobitant.call(this, species, name, gender, say);
+	this.legs = 2;
+	this.wings = 2;
+}
+
+Bird.prototype = Object.create(Inhobitant.prototype);
+
+Bird.prototype.getLimbs = function() {
+	return Inhobitant.prototype.getLimbs.call(this) + ` and ${this.wings} wings`;
 };
 
-const woman = new Human('human', 'Galyna', 'female', 'Dear, do you love me?', [
+const woman = new Human('', 'Galyna', 'female', 'Dear, do you love me?', [
 	'man',
 	'cat',
 ]);
 
-const man = new Human('human', 'Alex', 'male', 'Dear, I love borscht', [
+const man = new Human('', 'Alex', 'male', 'Dear, I love borscht', [
 	'woman',
 	'cat',
 ]);
@@ -68,6 +84,13 @@ const cat = new Animal('cat', 'Benjamin', 'male', 'meow-meow', [
 
 const dog = new Animal('dog', 'Charly', 'male', 'woof-woof');
 
-[woman, man, dog, cat].forEach(inhabitant => {
+const a = new Inhobitant('rhino', 'Asswell', 'malefe', 'bla-bla-bla', [
+	'Barmaley',
+	'Kakadu',
+]);
+
+const blueBird = new Bird('parrot', 'Kakadu', 'female', 'popka-durak');
+
+[woman, man, dog, cat, a, blueBird].forEach(inhabitant => {
 	print(inhabitant.getBio());
 });
