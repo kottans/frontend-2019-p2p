0fd1e59ae134eda6222f4a33e6de2ab58e2c4eb3
Arthur <gorbenkoartur.ur@gmail.com>
2019-06-09 09:34:13 +0300

    refactored with event.target usage

--- submissions/ArthurGorbenko/js-dom/README.md
+++ submissions/ArthurGorbenko/js-dom/README.md
@@ -1,1 +1,8 @@
 # js-dom-task
+**Practical task**: Implement interactive side-menu' site without page reloads. You may choose any theme: pokemons, phones, site pages aka about/prices/goal/etc.
+
+### The goal
+
+Main goal - menu with ability to load some predefined html content related to own menu item into right-positioned `<main>` content container.
+
+[Demo](https://arthurgorbenko.github.io/js-dom-task/)
--- submissions/ArthurGorbenko/js-dom/script.js
+++ submissions/ArthurGorbenko/js-dom/script.js
@@ -9,35 +9,35 @@ class puppie {
 }
 const puppies = [
   new puppie(
-    "Labrador",
+    "labrador",
     "100$",
     5,
     "Labradors are dogs for the family, they quickly become its active members, in their own way, but clearly participate in important and not very important events. They need to communicate in a literal sense, and the characteristic feature of a Labrador is that dogs do not disturb. ",
     "images/labrador.jpg"
   ),
   new puppie(
-    "Doberman",
+    "doberman",
     "150$",
     2,
     "Muscular, elegant and graceful dog. This breed is courageous, resourceful, courageous, very clever, with an excellent sense of smell and with an aggressive look. They are among the most respected and popular dog breeds; known for their deep devotion and defensive nature.",
     "images/doberman.jpg"
   ),
   new puppie(
-    "Chow chow",
+    "chow-chow",
     "120$",
     5,
     "The Chow is an independent, proud, imperturbable dog with dignity. They are quite reserved and suspicious of strangers, but for the owner it is a faithful and devoted companion and protector.",
     "images/chaochao.jpg"
   ),
   new puppie(
-    "Yorkshire Terrier",
+    "yorkshire-terrier",
     "170$",
     1,
     "Despite the miniature, Yorkshire terriers retain the qualities inherent in large terriers - courage, curiosity, tirelessness. He is friendly with people and with other dogs and loyal to the owner.",
     "images/terrier.jpg"
   ),
   new puppie(
-    "St. Bernard dog",
+    "st.Bernard-dog",
     "135$",
     4,
     "St. Bernard is an intelligent dog with a strong sense of orientation and smell. It is a quiet, calm, playful dog, not much barking. Affectionate to the family, he is friends with children and accepts strangers well, without being aggressive.",
@@ -45,20 +45,28 @@ const puppies = [
   )
 ];
 
-const buttons = document.querySelectorAll(".menu_button");
 const image = document.querySelector(".info_image");
 const parameters = document.querySelectorAll(".parameters");
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function() {
-    let j = 0;
-    for (prop in puppies[i]) {
-      if (j === 4) {
-        break;
-      }
-      parameters[j].textContent = prop + ": " + puppies[i][prop];
-      j++;
+const menu = document.querySelector(".menu");
+menu.addEventListener("click", function(event) {
+  let target = event.target;
+  while (target != menu) {
+    puppies.forEach(el =>
+      target.classList.contains(el.Breed) ? replace(target, el) : null
+    );
+    target = target.parentNode;
+  }
+});
+
+function replace(buttonElement, dogCard) {
+  let countForParameters = 0;
+  for (prop in dogCard) {
+    if (prop === "Source") {
+      break;
     }
-    image.src = puppies[i].Source;
-  });
+    parameters[countForParameters].textContent = prop + " : " + dogCard[prop];
+    countForParameters++;
+  }
+  image.src = dogCard.Source;
 }
