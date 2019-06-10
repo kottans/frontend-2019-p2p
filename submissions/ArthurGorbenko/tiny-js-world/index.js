71b97d83045cfe95d662a22b77ea9f916328504b
Arthur <gorbenkoartur.ur@gmail.com>
2019-06-10 16:37:48 +0300

    optimize calling print() function

--- submissions/ArthurGorbenko/tiny-js-world/index.js
+++ submissions/ArthurGorbenko/tiny-js-world/index.js
@@ -1,94 +1,64 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: https://github.com/ArthurGorbenko/a-tiny-JS-world/blob/gh-pages/index.js
-   Web app: https://arthurgorbenko.github.io/a-tiny-JS-world/
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
 const dog = {
-    type : "dog",
-    name : "Barley",
-    gender : "male",
-    legs : 4,
-    hasTail : true,
-    saying : "gav-gav"
-}
+  type: "dog",
+  name: "Barley",
+  gender: "male",
+  legs: 4,
+  hasTail: true,
+  saying: "gav-gav"
+};
 
 const cat = {
-    type : "cat",
-    name : "Rude",
-    gender : "male",
-    legs : 4,
-    hasTail : true,
-    saying : "Kottans are the best!"
-}
+  type: "cat",
+  name: "Rude",
+  gender: "male",
+  legs: 4,
+  hasTail: true,
+  saying: "Kottans are the best!"
+};
 
 const man = {
-    type : "human",
-    name : "Alex",
-    gender : "male",
-    legs : 2,
-    hasTail : false,
-    saying : "I want to start learning frontend development!"
-}
+  type: "human",
+  name: "Alex",
+  gender: "male",
+  legs: 2,
+  hasTail: false,
+  saying: "I want to start learning frontend development!"
+};
 
 const woman = {
-    type : "human",
-    name : "Meggy",
-    gender : "female",
-    legs : 2,
-    hasTail : false,
-    saying : "Hello,Alex."
-}
+  type: "human",
+  name: "Meggy",
+  gender: "female",
+  legs: 2,
+  hasTail: false,
+  saying: "Hello,Alex."
+};
 
 const womanCat = {
-    type : "human-cat",
-    name : "woman Cat",
-    gender : "female",
-    legs : 2,
-    hasTail : true,
-    saying : "Meow,Kottans are the best of the best!"
-}
+  type: "human-cat",
+  name: "woman Cat",
+  gender: "female",
+  legs: 2,
+  hasTail: true,
+  saying: "Meow,Kottans are the best of the best!"
+};
 
-function objectsToString (obj){
+[dog, cat, man, woman, womanCat].forEach(inhobitant =>
+  print(objectsToString(inhobitant))
+);
+
+function objectsToString(obj) {
     let objectAsString = "";
-    for (prop in obj){
-        if (prop === "saying"){
-            objectAsString +=prop + " : \"" + obj[prop]+ "\"";
-            break;
-        } else if (prop === "hasTail" || prop === "legs"){
+    for (prop in obj) {
+      if (prop === "saying") {
+        objectAsString += prop + ' : "' + obj[prop] + '"';
+        break;
+      } else if (prop === "hasTail" || prop === "legs") {
         objectAsString += prop + " : " + obj[prop] + ", ";
         continue;
-        }
-        objectAsString += obj[prop] + ", ";
+      }
+      objectAsString += obj[prop] + ", ";
     }
     return objectAsString + ";";
-}
-
-print(objectsToString(dog));
-print(objectsToString(cat));
-print(objectsToString(man));
-print(objectsToString(woman));
-print(objectsToString(womanCat));
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+  }
+  
