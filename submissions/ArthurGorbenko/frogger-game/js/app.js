5cd222353b7f49482de7065746deaf1116f4c0fc
Arthur <gorbenkoartur.ur@gmail.com>
2019-06-15 18:45:57 +0300

    first working version

---
+++ submissions/ArthurGorbenko/frogger-game/js/app.js
@@ -0,0 +1,129 @@
+
+let countForWonRound = 0;
+
+const blockWidth = 100;
+const blockHeight = 90;
+const fieldMaxHeight = 400;
+const fieldMinHeight = -70;
+const fieldMaxWidth = 405;
+const fieldMinWidth = 0;
+const waterLevel = fieldMinHeight + blockHeight;
+const startPositionX = 200;
+const startPositionY = 300;
+
+class Enemy {
+    constructor(x, y) {
+        this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+        this.speed = Math.floor((Math.random() * 200)) + 100;
+    }
+    update(dt) {
+        this.x += this.speed * dt;
+        
+        if (this.x > 600) {
+            this.x = -100;
+        }
+
+        const halfOfSizeImg = 50;
+
+        if (this.x < player.x + halfOfSizeImg &&
+            this.x + halfOfSizeImg > player.x &&
+            this.y < player.y + halfOfSizeImg &&
+            this.y + halfOfSizeImg > player.y) {
+            alert('You are busted!')
+            countForWonRound = 0;
+            player.x = startPositionX;
+            player.y = startPositionY;
+            allEnemies.forEach(el => el.generateSpeed());
+        }
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    generateSpeed(){
+        this.speed = Math.floor((Math.random() * 200)) + 100;
+    }
+}
+
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = "images/char-boy.png";
+
+    }
+    update(keyPressed) {
+        switch (keyPressed) {
+            case "left":
+                if (this.x - blockWidth <= fieldMinWidth) {
+                    return null;
+                } else {
+                    this.x -= blockWidth;
+                    break;
+                }
+            case "up":
+                if (this.y - blockHeight <= fieldMinHeight) {
+                    return null;
+                } else if (this.y - blockHeight < waterLevel) {
+                    countForWonRound++;
+                    if (countForWonRound === 1) {
+                        alert("Congratulations! You won " + countForWonRound + " round!");
+                    } else {
+                        alert("Congratulations! You won " + countForWonRound + " rounds!");
+                    }
+                    player.x = 200;
+                    player.y = 300;
+                    allEnemies.forEach(el => el.speed *= countForWonRound);// difficulty increase
+
+                }
+                else {
+                    this.y -= blockHeight;
+                    break;
+                }
+            case "right":
+                if (this.x + blockWidth >= fieldMaxWidth) {
+                    return null;
+                } else {
+                    this.x += blockWidth;
+                    break;
+                }
+            case "down":
+                if (this.y + blockHeight >= fieldMaxHeight) {
+                    return null;
+                } else {
+                    this.y += blockHeight;
+                };
+        }
+    }
+
+    handleInput(keyPressed) {
+        this.update(keyPressed);
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+let player = new Player(200, 400);
+let enemyOne = new Enemy(-50, 50);
+let enemyTwo = new Enemy(-50, 130);
+let enemyThree = new Enemy(-50, 210);
+
+const allEnemies = [enemyOne, enemyTwo, enemyThree];
+
+document.addEventListener('keyup', function (e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+
+
+
