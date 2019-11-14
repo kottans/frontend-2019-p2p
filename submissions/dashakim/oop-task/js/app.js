3cb07f66c70c4767ca94788e14e0c0ecfbd7b1e6
Daria Tabala <dasha.tabala@gmail.com>
2019-11-14 14:51:58 +0200

    Make changes after first review

--- submissions/dashakim/oop-task/js/app.js
+++ submissions/dashakim/oop-task/js/app.js
@@ -1,100 +1,106 @@
-//game score
-var score = 0
-//Enemy class with update(), render() methods
-var Enemy = function (x, y, speed) {
-    this.x = x
-    this.y = y
-    this.speed = speed
-    this.sprite = 'images/car.png';
-};
-var END_CANVAS = 510
-var BEGIN_CANVAS = -50
-Enemy.prototype.update = function (dt) {
+var gameScore = 0;
 
-    this.x += this.speed * dt
-    if (this.x > END_CANVAS) {
-        this.x = BEGIN_CANVAS
-        //random speed for enemies
-        this.speed = 100 + Math.floor(Math.random() * 222)
-    }
-    // return to start position player when collide with enemy
-    if (player.x > (this.x - 60) && player.x < (this.x + 60) && player.y > (this.y - 60) && player.y < (this.y + 60)) {
-        player.startPosition();
-    }
+const ENEMY = {
+  speed: 100,
+  varianceSpeed: 222,
+  location: [70, 155, 235]
+};
+const PLAYER = {
+  startX: 200,
+  startY: 404,
+  stepX: 101,
+  stepY: 83
 };
 
-Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+const CANVAS = {
+  begin: -60,
+  end: 510,
+  leftSide: 0,
+  rightSide: 400,
+  top: 0,
+  bottom: 404,
+  halfCell: 50
 };
 
-var allEnemies = []
-var enemyLocation = [60, 140, 220]
+var Draw = function() {};
 
-enemyLocation.forEach(function (y) {
-    allEnemies.push(new Enemy(0, y, 200))
-})
-
-//start location for player
-var START_PLAYER_X = 200
-var START_PLAYER_Y = 404
-
-//class player
-var Player = function () {
-    this.startPosition()
-    this.player = 'images/char-cat-girl.png'
-}
-//back to start
-Player.prototype.startPosition = function () {
-    this.x = START_PLAYER_X
-    this.y = START_PLAYER_Y
-}
-
-Player.prototype.update = function () {
-    if (this.y <= 0) {
-        this.startPosition()
-        score++
-        displayStats()
-    }
-}
-
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.player), this.x, this.y)
-}
-
-var LEFT_WALL = 0;
-var RIGHT_WALL = 400;
-var TOP_WALL = 0;
-var BOTTOM_WALL = 404;
-var PLAYER_HEIGHT = 101;
-var PLAYER_WIDTH = 83;
-
-// moves the player up, down, left, right
-Player.prototype.handleInput = function (keyUp) {
-    if (keyUp === 'left' && this.x > LEFT_WALL) {
-        this.x = this.x - PLAYER_HEIGHT;
-    } else if (keyUp === 'right' && this.x < RIGHT_WALL) {
-        this.x = this.x + PLAYER_HEIGHT;
-    } else if (keyUp === 'up' && this.y > TOP_WALL) {
-        this.y = this.y - PLAYER_WIDTH;
-    } else if (keyUp === 'down' && this.y < BOTTOM_WALL) {
-        this.y = this.y + PLAYER_WIDTH;
-    }
+Draw.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
-var player = new Player()
-
-var displayStats = function () {
-    document.getElementById('currentStats').innerHTML = 'Score: ' + score
+var Enemy = function(x, y, speed, player) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.player = player;
+  this.sprite = "images/car.png";
 };
 
-document.addEventListener('keyup', function (e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+Enemy.prototype = Object.create(Draw.prototype);
+Enemy.prototype.constructor = Enemy;
 
-    player.handleInput(allowedKeys[e.keyCode]);
+Enemy.prototype.update = function(dt) {
+  this.x += this.speed * dt;
+  if (this.x > CANVAS.end) {
+    this.x = CANVAS.begin;
+    //random speed for enemies
+    this.speed = ENEMY.speed + Math.floor(Math.random() * ENEMY.varianceSpeed);
+  }
+  // return to start position player when collide with enemy
+  if (
+    player.x > this.x - CANVAS.halfCell &&
+    player.x < this.x + CANVAS.halfCell &&
+    player.y > this.y - CANVAS.halfCell &&
+    player.y < this.y + CANVAS.halfCell
+  ) {
+    player.startPosition();
+  }
+};
+
+var Player = function() {
+  this.startPosition();
+  this.sprite = "images/char-cat-girl.png";
+};
+Player.prototype = Object.create(Draw.prototype);
+Player.prototype.constructor = Player;
+Player.prototype.update = function() {
+  if (this.y <= 0) {
+    displayStats();
+    this.startPosition();
+  }
+};
+
+Player.prototype.startPosition = function() {
+  this.x = PLAYER.startX;
+  this.y = PLAYER.startY;
+};
+
+Player.prototype.handleInput = function(keyUp) {
+  if (keyUp === "left" && this.x > CANVAS.leftSide) {
+    this.x = this.x - PLAYER.stepX;
+  } else if (keyUp === "right" && this.x < CANVAS.rightSide) {
+    this.x = this.x + PLAYER.stepX;
+  } else if (keyUp === "up" && this.y > CANVAS.top) {
+    this.y = this.y - PLAYER.stepY;
+  } else if (keyUp === "down" && this.y < CANVAS.bottom) {
+    this.y = this.y + PLAYER.stepY;
+  }
+};
+
+var displayStats = function() {
+  gameScore++;
+  document.getElementById("currentStats").innerHTML = "Score: " + gameScore;
+};
+
+var allEnemies = ENEMY.location.map(y => new Enemy(0, y, 200, player));
+var player = new Player();
+
+document.addEventListener("keyup", function(e) {
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down"
+  };
+  player.handleInput(allowedKeys[e.keyCode]);
 });
