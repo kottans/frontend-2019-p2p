77b7aadf7c84484b455152ca594d1be7ee2b394a
Daria Tabala <dasha.tabala@gmail.com>
2020-01-16 15:02:48 +0200

    Add semicolons to all files, fix index.js after review

--- submissions/dashakim/js-dom/index.js
+++ submissions/dashakim/js-dom/index.js
@@ -1,45 +1,45 @@
-const nav = document.querySelector('nav')
-const container = document.querySelector('.container')
-const bookList = document.createElement('UL')
-bookList.classList.add('book-list')
-nav.appendChild(bookList)
-const content = document.createElement('DIV')
-content.classList.add('content')
-container.appendChild(content)
+const nav = document.querySelector("nav");
+const container = document.querySelector(".container");
+const bookList = document.createElement("UL");
+bookList.classList.add("book-list");
+nav.appendChild(bookList);
+const content = document.createElement("DIV");
+content.classList.add("content");
+container.appendChild(content);
 
-const createElement = function (data) {
-    data.map(function (el) {
-        const title = document.createElement('LI')
-        title.classList.add('book-item')
+const createElement = function(data) {
+  data.map(function(el) {
+    const title = document.createElement("LI");
+    title.classList.add("book-item");
 
-        title.dataset.index = data.indexOf(el);
-        const name = document.createTextNode(el.author)
-        bookList.appendChild(title)
-        title.appendChild(name)
-    })
-}
+    title.dataset.index = data.indexOf(el);
+    const name = document.createTextNode(el.author);
+    bookList.appendChild(title);
+    title.appendChild(name);
+  });
+};
 
-createElement(storage)
+createElement(storage);
 
-nav.appendChild(bookList).firstChild.classList.add('active')
-const addContent = function (data) {
-    content.innerHTML = `
+nav.appendChild(bookList).firstChild.classList.add("active");
+const addContent = function(data) {
+  content.innerHTML = `
         <h2>${data.title}</h2>
       <p>${data.about}<br><br><strong>Price:${data.price}</strong></p>
       <img src="${data.cover}" alt="${data.title}" class="cover-book">
-      `
-}
+      `;
+};
 
-addContent(storage[0])
+addContent(storage[0]);
 
-bookList.onclick = function (event) {
-    let target = event.target // где был клик?
-    if (!target) return;
-    if (!bookList.contains(target)) return
-    if (document.querySelector('.active')) {
-        document.querySelector('.active').classList.remove('active');
-    }
-    target.classList.add('active');
-    let selected = storage[target.dataset.index];
-    addContent(selected)
-}
+const changePage = ({ target }) => {
+  if (!target || !bookList.contains(target)) return;
+  if (document.querySelector(".active")) {
+    document.querySelector(".active").classList.remove("active");
+  }
+  target.classList.add("active");
+  let selected = storage[target.dataset.index];
+  addContent(selected);
+};
+
+bookList.addEventListener("click", changePage);
