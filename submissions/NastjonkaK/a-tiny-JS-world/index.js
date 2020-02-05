26b774ad632b5ac5659c05d06b30eaf2c671b38c
Anastasiia Kruss <nastjonka88k@gmail.com>
2020-02-05 16:01:56 +0200

    Improved the code employing methods: Object.entries, Array.map, Array.join

--- submissions/NastjonkaK/a-tiny-JS-world/index.js
+++ submissions/NastjonkaK/a-tiny-JS-world/index.js
@@ -1,13 +1,3 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
 const dog = {
   type: "dog",
   name: "Charlie",
@@ -59,34 +49,9 @@ const womanCat = {
 };
 
 function objToString(obj) {
-  let str = "";
-  for (let p in obj) {
-    if (obj.hasOwnProperty(p)) {
-      str += p + ": " + obj[p] + "; ";
-    }
-  }
-  return str;
+  return Object.entries(obj)
+    .map(([key, value]) => `${key}: ${value}`)
+    .join("; ");
 }
 
-[dog, cat, man, woman, womanCat].forEach(el => {
-  print(objToString(el));
-});
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+[dog, cat, man, woman, womanCat].forEach(el => print(objToString(el)));
