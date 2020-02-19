9eb20085f792e2fa47cf243072fa90bed95a04fc
Daria Tabala <35972351+dashakim@users.noreply.github.com>
2020-02-19 13:00:41 +0200

    Add task (#106)
    * Add task
    
    * Fix css style for mobile version
    
    * Add semicolons to all files, fix index.js after review

---
+++ submissions/dashakim/js-dom/storage.js
@@ -0,0 +1,43 @@
+const storage = [
+  {
+    author: "Brian Tracy",
+    title: "Eat That Frog",
+    about:
+      "Mark Twain once said, “Eat a live frog first thing in the morning and nothing worse will happen to you the rest of the day.”<br> Want to do more than just get more done? How about get what matters done so that you are making quantum leaps in your business? This book makes too much sense and has changed the way I plan and prioritize my day.  Put it this way, I eat my frog in the morning, and I focus on the top three things that only I can do in my business a whole lot more.<br> Bestselling author Brian Tracy cuts to the core of what is vital to effective time management: decision, discipline, and determination. In this fully revised and updated second edition, he provides brand new information on how to keep technology from dominating your time. He details twenty-one practical and doable steps that will help you stop procrastinating and get more of the important tasks done—today!",
+    price: "$18.69 Hardcover",
+    cover: "src/book_frog.jpg"
+  },
+  {
+    author: "Chris Brogan and Julien Smith",
+    title: "The Impact Equation by Chris Brogan and Julien Smith",
+    about:
+      "In The Impact Equation, Brogan and Smith show that to make people truly care about what you have to say, you need more than just a good idea, trust among your audience, or a certain number of fol­lowers. You need a potent mix of all of the above and more. The Impact Equation will give you the tools and metrics that guarantee your message will be heard.<br>Use the Impact Equation to figure out what you’re doing right and wrong. Apply it to a blog, a tweet, a video, or a mainstream-media advertising cam­paign. Use it to explain why a feature in a national newspaper that reaches millions might have less impact than a blog post that reaches a thousand passionate subscribers.",
+    price: "$5.89 Hardcover",
+    cover: "src/book_impact.jpg"
+  },
+  {
+    author: "Carol S. Dweck",
+    title: "Mindset: The New Psychology of Success",
+    about:
+      "After decades of research, world-renowned Stanford University psychologist Carol S. Dweck, Ph.D., discovered a simple but groundbreaking idea: the power of mindset. In this brilliant book, she shows how success in school, work, sports, the arts, and almost every area of human endeavor can be dramatically influenced by how we think about our talents and abilities. People with a fixed mindset — those who believe that abilities are fixed — are less likely to flourish than those with a growth mindset — those who believe that abilities can be developed. Mindset reveals how great parents, teachers, managers, and athletes can put this idea to use to foster outstanding accomplishment.",
+    price: "$10.26 Paperback",
+    cover: "src/book_mindset.jpg"
+  },
+  {
+    author: "Mortimer J. Adler, Charles Van Doren",
+    title: "How to Read a Book: The Classic Guide to Intelligent Reading",
+    about:
+      "How to Read a Book, originally published in 1940, has become a rare phenomenon, a living classic. It is the best and most successful guide to reading comprehension for the general reader. And now it has been completely rewritten and updated. You are told about the various levels of reading and how to achieve them – from elementary reading, through systematic skimming and inspectional reading, to speed reading, you learn how to pigeonhole a book, X-ray it, extract the author's message, criticize. You are taught the different reading techniques for reading practical books, imaginative literature, plays, poetry, history, science and mathematics, philosophy and social science.",
+    price: "$11.99",
+    cover: "src/book_how.jpg"
+  },
+  {
+    author: "Susan Wise Bauer",
+    title:
+      "The Well-Educated Mind: A Guide to the Classical Education You Never Had",
+    about:
+      "Have you lost the art of reading for pleasure? Are there books you know you should read but haven't because they seem too daunting? In The Well-Educated Mind, Susan Wise Bauer provides a welcome and encouraging antidote to the distractions of our age, electronic and otherwise. In her previous book, The Well-Trained Mind, the author provided a road map of classical education for parents wishing to home-school their children, and that book is now the premier resource for home-schoolers. In this new book, Bauer takes the same elements and techniques and adapts them to the use of adult readers who want both enjoyment and self-improvement from the time they spend reading.",
+    price: "$18.32 Hardcover",
+    cover: "src/book_educated.jpg"
+  }
+];
