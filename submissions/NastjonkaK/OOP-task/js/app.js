04563bafdb2d5dc68aeb5b5500a7932e5ca719c0
Anastasiia Kruss <nastjonka88k@gmail.com>
2020-08-22 15:16:58 +0300

    Rewrote constant values, created general class, optimised some code

--- submissions/NastjonkaK/OOP-task/js/app.js
+++ submissions/NastjonkaK/OOP-task/js/app.js
@@ -1,117 +1,110 @@
 // Enemies our player must avoid
-let points = 0;
-let scoreboard = document.createElement('p');
-scoreboard.textContent = 'Your score: 0';
-scoreboard.style.cssText = 'color: #3a9500; font-size: 30px; font-weight: bold; margin-top: 50px';
+const scoreboard = document.createElement('p');
+const renderScore = points => { scoreboard.innerHTML = `Your score: ${points}` };
+scoreboard.classList.add('scoreboard');
 document.body.appendChild(scoreboard);
+let points = 0;
+renderScore(points);
 
-const gameField = {
-    minX: 0,
-    minY: 0,
-    max: 400,
-    width: 500,
-    cellWidth: 100,
-    cellHeight: 80,
+const GAME_FIELD = {
+    MIN_X: 0,
+    MIN_Y: 0,
+    MAX: 400,
+    WIDTH: 500,
+    CELL_WIDTH: 100,
+    CELL_HEIGHT: 80,
 };
 
-const enemyStartPosition = {
-    enemyStartX: -100,
-    firstEnemyStartY: 65,
-    secondEnamyStartY: 145,
-    thirdEnemyStartY: 230,
-    enemyMaxSpeed: 100,
-    enemyMinSpeed: 50
+const ENEMY_CONFIG = {
+    ENEMY_START_X: -100,
+    FIRST_ENEMY_START_Y: 65,
+    SECOND_ENEMY_START_Y: 145,
+    THIRD_ENEMY_START_Y: 230,
+    ENEMY_MAX_SPEED: 100,
+    ENEMY_MIN_SPEED: 50
 };
 
-const playerStartPosition = {
-    playerStartX: 200,
-    playerStartY: 400
+const PLAYER_START_POSITION = {
+    PLAYER_START_X: 200,
+    PLAYER_START_Y: 400
 };
 
-const Enemy = function (y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = enemyStartPosition.enemyStartX;
-    this.y = y;
-    this.speed = Math.floor(Math.random() * (enemyStartPosition.enemyMaxSpeed - enemyStartPosition.enemyMinSpeed) + enemyStartPosition.enemyMinSpeed);
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function (dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += this.speed * dt;
-    if (this.x > gameField.width) {
-        this.x = enemyStartPosition.enemyStartX;
+class Personage {
+    constructor(x, y, sprite) {
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
     }
-    if ((Math.round(this.x / gameField.cellWidth) === Math.round(player.x / gameField.cellWidth)) &&
-        (Math.round(this.y / gameField.cellHeight) === Math.round(player.y / gameField.cellHeight))) {
-        player.restart();
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-};
+}
 
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-const Player = function () {
-    this.x = playerStartPosition.playerStartX;
-    this.y = playerStartPosition.playerStartY;
-    this.sprite = 'images/char-boy.png';
-};
-
-Player.prototype.update = function () {
-    if (this.y < gameField.minY) {
-        points++;
-        scoreboard.innerHTML = `Your score: ${points}`;
-        this.x = playerStartPosition.playerStartX;
-        this.y = playerStartPosition.playerStartY;
+class Enemy extends Personage {
+    constructor(x, y, player, sprite = 'images/enemy-bug.png') {
+        super(x, y, sprite);
+        this.speed = Math.floor(Math.random() * (ENEMY_CONFIG.ENEMY_MAX_SPEED - ENEMY_CONFIG.ENEMY_MIN_SPEED) + ENEMY_CONFIG.ENEMY_MIN_SPEED);
+        this.player = player;
     }
-};
 
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    update(dt) {
+        this.x += this.speed * dt;
+        if (this.x > GAME_FIELD.WIDTH) {
+            this.x = ENEMY_CONFIG.ENEMY_START_X;
+        }
+        this.contactCheck();
+    }
 
-Player.prototype.handleInput = function (key) {
-    if (key == 'left' && this.x > gameField.minX) {
-        this.x -= gameField.cellWidth;
-    } else if (key == 'right' && this.x < (gameField.width - gameField.cellWidth)) {
-        this.x += gameField.cellWidth;
-    } else if (key == 'up') {
-        this.y -= gameField.cellHeight;
-    } else if (key == 'down' && this.y < gameField.max) {
-        this.y += gameField.cellHeight;
-    };
-};
+    contactCheck() {
+        if ((Math.round(this.x / GAME_FIELD.CELL_WIDTH) === Math.round(this.player.x / GAME_FIELD.CELL_WIDTH)) &&
+            (Math.round(this.y / GAME_FIELD.CELL_HEIGHT) === Math.round(this.player.y / GAME_FIELD.CELL_HEIGHT))) {
+            points = 0;
+            renderScore(points);
+            this.player.x = PLAYER_START_POSITION.PLAYER_START_X;
+            this.player.y = PLAYER_START_POSITION.PLAYER_START_Y;
+        }
+    }
+}
 
-Player.prototype.restart = function () {
-    points = 0;
-    scoreboard.innerHTML = `Your score: ${points}`;
-    this.x = playerStartPosition.playerStartX;
-    this.y = playerStartPosition.playerStartY;
+class Player extends Personage {
+    constructor(x, y, sprite = 'images/char-boy.png') {
+        super(x, y, sprite);
+    }
 
-};
+    update(dt) {
+        this.increasePoints();
+    }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-let enemyFirst = new Enemy(enemyStartPosition.firstEnemyStartY);
-let enemySecond = new Enemy(enemyStartPosition.secondEnamyStartY);
-let enemyThird = new Enemy(enemyStartPosition.thirdEnemyStartY);
-let allEnemies = [enemyFirst, enemySecond, enemyThird];
-let player = new Player();
+    handleInput(key) {
+        if (key == 'left' && this.x > GAME_FIELD.MIN_X) {
+            this.x -= GAME_FIELD.CELL_WIDTH;
+        } else if (key == 'right' && this.x < (GAME_FIELD.WIDTH - GAME_FIELD.CELL_WIDTH)) {
+            this.x += GAME_FIELD.CELL_WIDTH;
+        } else if (key == 'up') {
+            this.y -= GAME_FIELD.CELL_HEIGHT;
+        } else if (key == 'down' && this.y < GAME_FIELD.MAX) {
+            this.y += GAME_FIELD.CELL_HEIGHT;
+        };
+    }
+
+    increasePoints() {
+        if (this.y < GAME_FIELD.MIN_Y) {
+            points++;
+            renderScore(points);
+            this.x = PLAYER_START_POSITION.PLAYER_START_X;
+            this.y = PLAYER_START_POSITION.PLAYER_START_Y;
+        }
+    }
+
+}
+
+let player = new Player(PLAYER_START_POSITION.PLAYER_START_X, PLAYER_START_POSITION.PLAYER_START_Y);
+const enemyFirst = new Enemy(ENEMY_CONFIG.ENEMY_START_X, ENEMY_CONFIG.FIRST_ENEMY_START_Y, player);
+const enemySecond = new Enemy(ENEMY_CONFIG.ENEMY_START_X, ENEMY_CONFIG.SECOND_ENEMY_START_Y, player);
+const enemyThird = new Enemy(ENEMY_CONFIG.ENEMY_START_X, ENEMY_CONFIG.THIRD_ENEMY_START_Y, player);
+const allEnemies = [enemyFirst, enemySecond, enemyThird];
+
 
 
 // This listens for key presses and sends the keys to your
