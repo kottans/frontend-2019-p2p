9d9b78b4647b6e7f12f046873cf1f8425d924f8e
Chris Voitova <voitova@teamharbour.com>
2020-09-02 16:09:44 +0300

    code rewiev fixes

--- submissions/kris-almort/a-tiny-js-world/index.js
+++ submissions/kris-almort/a-tiny-js-world/index.js
@@ -74,16 +74,18 @@ const catWoman = {
 function preparePrint(obj) {
   return `
    <strong>${obj.species}</strong>;
-	name: <strong>${obj.name}</strong>;
-	gender: ${obj.gender}; 
-	legs: ${obj.legs};
-	${obj.hasOwnProperty("hands") ? `hands: ${obj.hands};` : ""}
-	saying: ${obj.saying};
-	${obj.hasOwnProperty("friends") ? `friends: ${obj.friends};` : ""}`;
+   name: <strong>${obj.name}</strong>;
+   gender: ${obj.gender}; 
+   legs: ${obj.legs};
+   ${obj.hasOwnProperty("hands") ? `hands: ${obj.hands};` : ""}
+   saying: ${obj.saying};
+   ${
+     obj.hasOwnProperty("friends") && obj.friends.length > 0
+       ? `friends: ${obj.friends};`
+       : ""
+   }`;
 }
 
-print(preparePrint(cat), "div");
-print(preparePrint(dog), "div");
-print(preparePrint(man), "div");
-print(preparePrint(woman), "div");
-print(preparePrint(catWoman), "div");
+[cat, dog, man, woman, catWoman].forEach((item) =>
+  print(preparePrint(item), "div")
+);
