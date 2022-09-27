623614c7ef8af2e6185780ed439b3487605babfd
Dania Buriachenko <buriachenko923@gmail.com>
2022-09-28 00:25:13 +0300

    change let to const and re-name variables

--- submissions/DaniaB24/memory-pair-game/js/app.js
+++ submissions/DaniaB24/memory-pair-game/js/app.js
@@ -1,40 +1,36 @@
-const allCard = document.querySelector(".board_game");
+const allCards = document.querySelector(".board_game");
 let firstCard, secondCard;
 let lockGame = false;
-let allPairCard = 6;
-let pairCard = 0;
-let allPlayerCard = [
+const allPairCards = 6;
+const pairCards = 0;
+const allPlayerCards = [
   {
     id: "dewey",
-    imgFront: "images/logomcduck.png",
     imgBack: "images/dewey.png",
   },
   {
     id: "louie",
-    imgFront: "images/logomcduck.png",
     imgBack: "images/louie.png",
   },
   {
     id: "huey",
-    imgFront: "images/logomcduck.png",
     imgBack: "images/huey.png",
   },
   {
     id: "bentina",
-    imgFront: "images/logomcduck.png",
     imgBack: "images/bentina.png",
   },
   {
     id: "della",
-    imgFront: "images/logomcduck.png",
     imgBack: "images/della.png",
   },
   {
     id: "mcQuack",
-    imgFront: "images/logomcduck.png",
     imgBack: "images/mcQuack.png",
   },
 ];
+const memoryCards = [...allPlayerCards, ...allPlayerCards];
+
 function flipCard(e) {
   const targetCard = e.target.closest(".memory_card");
   if (lockGame || !targetCard || targetCard === firstCard) return;
@@ -49,39 +45,35 @@ function flipCard(e) {
     checkMatch();
   }
 }
-const memoryCards = [...allPlayerCard, ...allPlayerCard];
-function sortCard() {
+function sortCards() {
   memoryCards.sort(function () {
     return 0.5 - Math.random();
   });
 }
-function createCard() {
-  sortCard();
-
-  let clonedCard = memoryCards.map((item) => {
+function createCards() {
+  sortCards();
+  const clonedCards = memoryCards.map((item) => {
     return `
     <div class="memory_card" id=${item.id}>
       <img src=${item.imgBack} class="back_card">
-      <img src=${item.imgFront} class="front_card">
+      <img src="images/logomcduck.png" class="front_card">
     </div>
 `;
   });
-  clonedCard.forEach((item) => {
-    allCard.insertAdjacentHTML("beforeend", item);
+  clonedCards.forEach((item) => {
+    allCards.insertAdjacentHTML("beforeend", item);
   });
 }
-createCard();
-
 function checkMatch() {
   if (firstCard.id === secondCard.id) {
-      disableCard();
-    pairCard++;
+    disableCards();
+    pairCards++;
     checkFinish();
   } else {
-    freezeCard();
+    freezeCards();
   }
 }
-function disableCard() {
+function disableCards() {
   lockGame = true;
   setTimeout(() => {
     lockGame = false;
@@ -91,23 +83,23 @@ function disableCard() {
     secondCard.classList.add("hide");
   }, 800);
 }
-function freezeCard() {
+function freezeCards() {
   lockGame = true;
   setTimeout(() => {
     lockGame = false;
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
-    firstCard = "";
-    secondCard = "";
+    firstCard = null;
+    secondCard = null;
   }, 900);
 }
 function checkFinish() {
   setTimeout(() => {
-    if (pairCard === allPairCard) {
+    if (pairCards === allPairCards) {
       alert("Congratulations!!!");
       location.reload();
     }
   }, 800);
 }
-
-allCard.addEventListener("click", flipCard);
+allCards.addEventListener("click", flipCard);
+createCards();
